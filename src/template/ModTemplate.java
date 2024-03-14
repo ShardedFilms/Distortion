d2cb9e1fc7df3558ffea46da409ea201dc7462c5
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-03-14 20:00:03 +0700

    Eval

--- src/template/ModTemplate.java
+++ src/template/ModTemplate.java
@@ -1,23 +1,31 @@
 package template;
 
 import arc.*;
+import arc.util.*;
 import mindustry.*;
 import mindustry.mod.*;
 import mindustry.game.EventType.*;
+
 
 import template.gen.*;
 
 public class ModTemplate extends Mod{
     static Clump activeState;
     public ModTemplate() {
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                new TimeDelta();
+                //activeState.init();
+                activeState = new AtlasConfig();
+                //if (activeState != null) {
+                activeState.init();
+                //}
+            });
+        });
 
-        new TimeDelta();
-        //activeState.init();
-        activeState = new AtlasConfig();
-        //if (activeState != null) {
-            activeState.init();
-        //}
-        ;
+
         Events.on(FileTreeInitEvent.class, e -> Core.app.post(() -> {
             FlameTest.updateTest();
             if (!Vars.headless) {
