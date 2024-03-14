7257ee006899b9e0fbcb5dd245579fd2db699be3
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-03-14 15:54:29 +0700

    Fig

--- src/template/ModTemplate.java
+++ src/template/ModTemplate.java
@@ -5,19 +5,14 @@ import template.gen.*;
 
 public class ModTemplate extends Mod{
     static Clump activeState;
-    static int lol =1;
     @Override
     public void loadContent(){
         EntityRegistry.register();
     }
     static void loadState(){
-        switch(lol){
-            case 1 -> activeState = new AtlasConfig();
-        }
-
-        if(activeState != null){
+            activeState = new AtlasConfig();
             activeState.init();
-        }
+
     }
 
 }
