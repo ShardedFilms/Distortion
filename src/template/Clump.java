1213997f6fed0467fdfafa5620d676d6ab2f6db3
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-03-14 15:12:09 +0700

    .

---
+++ src/template/Clump.java
@@ -0,0 +1,11 @@
+package template;
+
+public abstract class Clump {
+    //Load
+    public void update(){}
+    public void draw(){}
+    public void init(){}
+    public void loadAssets(){}
+    public void loadClient(){}
+
+}
