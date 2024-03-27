e4c5fb63eac69e4c102ad0696872cfc28d37cf62
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-03-27 15:59:36 +0700

    h

--- src/template/ModTemplate.java
+++ src/template/ModTemplate.java
@@ -17,18 +17,10 @@ import template.gen.*;
 public class ModTemplate extends Mod{
 
     public float time = 0f;
-    public float interval = 5f;
+    public float interval = 1f;
     public ModTemplate() {
-        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            BaseDialog dialog = new BaseDialog("entr");
-            dialog.cont.add("runnin").row();
-            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-            dialog.cont.button("Oke", dialog::hide).size(100f, 50f);
-            dialog.show();
-            //new AtlasConfig();
             new TimeDelta();
-            //activeState.update();
             run();
         });
     }
